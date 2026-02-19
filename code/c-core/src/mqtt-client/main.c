899cbd7e6f94b3124f001e1938be26238ca96e38
JoaoNasMonteiro <joaonasmont@gmail.com>
2026-02-19 17:00:34 +0000

    Started to refactor core business logic of MQTT client main.c

--- code/c-core/src/mqtt-client/main.c
+++ code/c-core/src/mqtt-client/main.c
@@ -14,8 +14,10 @@
 // Boilerplate stuff
 #define ARENA_SIZE (64 * 1024)
 #define BUFFER_SIZE 1024
-#define LOG_FIFO_NAME "/tmp/test_fifo"
 static uint8_t client_memory[ARENA_SIZE];
+
+#define PUB_FIFO_NAME "/tmp/mqtt_pub_fifo"
+#define cmd_FIFO_NAME "/tmp/mqtt_cmd_fifo"
 
 #define MODULE_NAME "MQTT_CLIENT"
 #include "../../include/logging.h"
@@ -45,6 +47,99 @@ int initialize_log_fifo(IPC_Channel *channel);
  *
  * Este será um daemon que vai rodar no background
  * */
+
+ int main() {
+  signal(SIGINT, intHandler);
+  signal(SIGTERM, intHandler);
+
+  // init arena
+  Arena arena;
+  arena_init(&arena, client_memory, ARENA_SIZE);
+
+  //init Pub and command (sub) channels 
+  IPC_Channel pub_channel = {0};
+  pub_channel.fd = -1;
+  if (ipc_open_channel(&pub_channel, PUB_FIFO_NAME) != 0) {
+    LOG_ERROR("Failed to open pub FIFO at %s", PUB_FIFO_NAME);
+    return -1;
+  }
+
+  IPC_Channel cmd_channel = {0};
+  cmd_channel.fd = -1;
+  if (ipc_open_channel(&cmd_channel, cmd_FIFO_NAME) != 0) {
+    LOG_ERROR("Failed to open command FIFO at %s", cmd_FIFO_NAME);
+    return -1;
+  }
+
+  // MQTT client initialization
+  LOG_DEBUG("Starting MQTT Client");
+  mqttContext *ctx = mqtt_create(ADDRESS, CLIENTID, 20, &arena, &cmd_channel, &pub_channel);
+  if (ctx == NULL) {
+    LOG_ERROR("Failed to create MQTT client");
+    return -1;
+  }
+
+
+  // Buffer allocation for reading from FIFO pipes
+  char *charbuffer = arena_alloc(&arena, BUFFER_SIZE);
+  if (charbuffer == NULL) {
+    LOG_ERROR("Failed to allocate memory from arena");
+    return -1;
+  }
+  memset(charbuffer, 0, BUFFER_SIZE);
+
+  char *payload = arena_alloc(&arena, BUFFER_SIZE);
+  if (payload == NULL) {
+    LOG_ERROR("Failed to allocate memory from arena");
+    return -1;
+  }
+  
+
+  ipc_read_nonblocking(&log_channel, NULL,
+                       BUFFER_SIZE); // Clear any existing messages
+
+  while (keepRunning) {
+
+    // Implementar lógica de loop central com polling dos pipes de IPC para evitar busy waiting 
+    // Try to resolve the message from the FIFO pipe
+    int bytesRead =
+        ipc_read_nonblocking(&log_channel, charbuffer, BUFFER_SIZE - 1);
+    if ((bytesRead > 0) && (charbuffer[0] != '\0')) {
+      charbuffer[bytesRead] = '\0'; // Null-terminate the string
+      LOG_INFO("Read message from FIFO: %s", charbuffer);
+
+      strncpy(payload, charbuffer, BUFFER_SIZE - 1);
+      payload[bytesRead - 1] = '\0'; // Remove newline character
+
+      LOG_INFO("Updated payload to: %s", payload);
+      // change later to get a better handling of FIFO pipes
+      memset(charbuffer, 0, BUFFER_SIZE);
+
+    } else {
+      LOG_INFO("Nothing to publish. Publishing Hello, World");
+      strncpy(payload, PAYLOAD, BUFFER_SIZE - 1);
+    }
+
+    // actually try to publish the message
+    if (mqtt_pub_message(ctx, TOPIC, payload) != 0) {
+      LOG_ERROR("Failed to publish message");
+    } else {
+      LOG_INFO("Successfully published message to topic %s: %s", TOPIC,
+               payload);
+    }
+    sleep(5);
+  }
+
+  LOG_DEBUG("Shutting down MQTT Client");
+  mqtt_disconnect_and_free(ctx);
+  arena_reset(&arena);
+  ipc_close_channel(&log_channel);
+
+  LOG_DEBUG("MQTT Client stopped");
+  return 0;
+}
+
+
 
 int main() {
   signal(SIGINT, intHandler);
